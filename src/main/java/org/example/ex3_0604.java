33c5b461f1cb649ce9465c6c6c232939f52b391d
yeosang0508 <yeosang0508@gmail.com>
2024-06-05 00:10:27 +0900

    ex3_0604

---
+++ src/main/java/org/example/ex3_0604.java
@@ -0,0 +1,39 @@
+package org.example;
+// 문제 : 아래와 같이 출력 되도록 해주세요.
+public class ex3_0604 {
+    public static void main(String[] args) {
+        // 예시 시작
+        int c = 20 + Math.더하기(10, 20);
+        System.out.println(c);
+        // 예시 끝
+
+        int 결과1 = Math.oneToSum(3);
+        System.out.println("결과1 : " + 결과1);
+        // 출력 : 결과1 : 6
+
+        int 결과2= Math.oneToSum(10);
+        System.out.println("결과2 : " + 결과2);
+        // 출력 : 결과2 : 55
+    }
+}
+
+class Math {
+    // static => 본사직원
+    // static은 현재 클래스에 지금당장 존재한다.
+    static int 더하기(int a, int b) {
+        // 메인 메서드의 int c와 아래 int c는 전혀 관계가 없다.
+        int c = a + b;
+
+        // return 값의 타입과 함수명의 앞부분이 같아야 한다.
+        // return => 변신
+        return c;
+    }
+    static int oneToSum(int a){
+        int sum = 0;
+        for(int i = 1; i <= a; i++){
+            sum += i;
+        }
+        return sum;
+    }
+
+}
